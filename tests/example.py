c512629d844b546631cbaeb5c761531393286f5d
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2023-12-03 18:40:17 +0100

    move tests to a separate dir

--- tests/example.py
+++ tests/example.py
@@ -1,14 +1,12 @@
 import iio
 import drunet
 
-
-### Test on a RGB image ###
+### Test on a RGB image
 a = iio.read("tractor_g20.png")
 out = drunet.denoise(a, 20)
 iio.write("denoised_tractor_g20.png", out)
 
-### Test on a grayscale image ###
+### Test on a grayscale image
 a = iio.read("gtractor_g20.png")
 out = drunet.denoise(a, 20)
 iio.write("gdenoised_tractor_g20.png", out)
-
